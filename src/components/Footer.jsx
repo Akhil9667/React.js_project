985a5808107eea908dfe712c5a96ef70138b505b
Akhil9667 <82561587+Akhil9667@users.noreply.github.com>
2021-06-27 23:01:37 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer(){
+  return(
+   <footer>
+     <p>
+     Copyright by Akhil @ {new Date().getFullYear()}
+   </p>
+   </footer>
+  );
+}
+
+export default Footer;
